0ff919a7620adccff8e9116ad2996eacbe4c423a
yousefalfoqaha <yousefmustafaadam@gmail.com>
2024-11-23 13:08:21 +0300

    quick sort algorithm

--- quick-sort/index.cpp
+++ quick-sort/index.cpp
@@ -3,10 +3,54 @@
 
 using namespace std;
 
-void quicksort(int* A, )
+void swapPointers(int *A, int p1, int p2) {
+  int temp = A[p1];
+  A[p1] = A[p2];
+  A[p2] = temp;
+}
+
+void quicksort(int *A, int lowIndex, int highIndex) {
+  if (lowIndex >= highIndex) {
+    return;
+  }
+
+  int pivot = A[highIndex];
+  int leftPointer = lowIndex, rightPointer = highIndex;
+
+  while (leftPointer < rightPointer) {
+    while (A[leftPointer] <= pivot && leftPointer < rightPointer) {
+      leftPointer++;
+    }
+    // cout << "left: " << A[leftPointer] << " ";
+    while (A[rightPointer] >= pivot && leftPointer < rightPointer) {
+      rightPointer--;
+    }
+    // cout << "right: " << A[rightPointer] << " ";
+    swapPointers(A, leftPointer, rightPointer);
+  }
+  swapPointers(A, leftPointer, highIndex);
+
+  quicksort(A, lowIndex, leftPointer - 1);
+  quicksort(A, leftPointer + 1, highIndex);
+}
+
 
 int main() {
+  int size = 10;
+  int A[size] = {8, 12, 1, 54, 55, 31, 41, 3, 77, 2};
 
+  cout << "Unsorted: ";
+  for (int i = 0; i < size; i++) {
+    cout << A[i] << " ";
+  }
+  cout << endl;
+
+  quicksort(A, 0, size - 1);
+
+  cout << "Sorted: ";
+  for (int i = 0; i < size; i++) {
+    cout << A[i] << " ";
+  }
 
   return 0;
 }
